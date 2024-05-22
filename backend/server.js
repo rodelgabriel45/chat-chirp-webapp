fe96ed6ea0197b8ee217e6509e5094a52cbdc792
rodelgabriel45@gmail.com <rodelgabriel45@gmail.com>
2024-05-23 02:36:05 +0800

    add config for deployment

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
 import cookieParser from "cookie-parser";
+import path from "path";
 
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
@@ -9,6 +10,8 @@ import userRoutes from "./routes/user.route.js";
 import { app, server } from "./socket/socket.js";
 
 const PORT = process.env.PORT || 3000;
+
+const __dirname = path.resolve();
 
 dotenv.config();
 
@@ -18,6 +21,11 @@ app.use(cookieParser());
 app.use("/api/auth", authRoutes);
 app.use("/api/message", messageRoutes);
 app.use("/api/user", userRoutes);
+
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
 
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
